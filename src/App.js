5c74927218e4122f3909cef1ff339ac02661f757
Alexander <ashuaibu@getcarbon.co>
2019-11-28 16:10:08 +0100

    [add] animation to login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { LandingPage } from './components/landingPage/components';
+import LoginPage from './components/login/components';
 
 const App = () => (
     <Router>
         <Switch>
             <Route exact path="/" component={LandingPage} />
-            <Route exact path="/login" component={LandingPage} />
+            <Route exact path="/login" component={LoginPage} />
         </Switch>
     </Router>
 );
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 // import PropTypes from 'prop-types';
 import './style.css';
+import { particleParameters } from './constants';
+
+// style for the item which we put over the animation
+const overlay = {
+    alignItems: 'center',
+    color: 'white',
+    display: 'flex',
+    fontSize: '20px',
+    height: '100%',
+    justifyContent: 'center',
+};
+
+// style for the particle which we want to display
+const particleStyle = {
+    height: '100%',
+    left: 0,
+    paddingBottom: '3%',
+    paddingTop: '3%',
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+};
 
 /**
  * Default layout for pages
@@ -16,7 +39,11 @@ const withLayout = (ComponentOne = 'div', ComponentTwo = 'div') => () => (
             <ComponentOne />
         </div>
         <div className="blueSection -blue">
-            <ComponentTwo />
+            <ComponentTwo style={overlay} />
+            <Particles
+                params={particleParameters}
+                style={particleStyle}
+            />
         </div>
     </div>
 );
