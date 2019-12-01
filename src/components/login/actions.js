986bd7c85f1fd40dbf6043b3a8eb9ea1253d1cbe
Alexander <ashuaibu@getcarbon.co>
2019-12-01 17:48:10 +0100

    [add]authentcation to admin

--- src/components/login/actions.js
+++ src/components/login/actions.js
@@ -1,19 +1,100 @@
-import { LOGIN_USER, AUTHENTICATE_USER } from './actionTypes';
+import {
+    LOGIN_USER, AUTHENTICATE_USER, TOGGLE_LOADING_NIN,
+    TOGGLE_CONFFIRMATION_CODE, CONFIRM_CODE, AUTHENTICATE_USER_STATUS, IS_USER_LOGGEDIN
+} from './actionTypes';
 
-const loginUser = password => {
-    const stringPasswordWithNumber = `+234${password}`;
-    return {
-        payload: stringPasswordWithNumber,
-        type: LOGIN_USER,
-    };
-};
+/**
+ * Triggers request to send a code to a phone number
+ *
+ * @function
+ * @return {Object} The {@link actionTypes.LOGIN_USER LOGIN_USER} action.
+ */
+const loginUser = phoneNumber => ({
+    payload: phoneNumber,
+    type: LOGIN_USER,
+});
 
+/**
+ * Triggers request to NIN API and confirm that user is indeed a citizen
+ *
+ * @function
+ * @return {Object} The {@link actionTypes.AUTHENTICATE_USER AUTHENTICATE_USER} action.
+ */
 const authenticateUser = payload => {
-    const password = `+234${payload.phoneNumber}`;
+    // convert the password to string for easy slicing and nomenclature
+    const stringPassword = String(payload.phoneNumber);
+    const phoneNumber = `+234${stringPassword.slice(1)}`;
     const { nin } = payload;
+
+    // return the actiontype and the password which has been appended with country code
     return {
-        payload: { nin, password },
+        payload: { nin, phoneNumber },
         type: AUTHENTICATE_USER,
     };
 };
-export default { authenticateUser, loginUser };
+
+/** Triggers a function which spins a loader indicating loading action
+ *
+ * @function
+ * @return {Object} The {@link actionTypes.TOGGLE_LOADING_NIN TOGGLE_LOADING_NIN} action.
+ */
+const loadingReducer = truthyvalue => ({
+    payload: truthyvalue,
+    type: TOGGLE_LOADING_NIN,
+});
+
+/** Triggers a function which changes a state variable
+ * this then indicates that the user needs to input a confirmation code
+ * and it opens that section on the interface
+ *
+ * @function
+ * @return {Object} The {@link actionTypes.TOGGLE_LOADING_NIN TOGGLE_LOADING_NIN} action.
+ */
+const confirmationCodeSection = truthyvalue => ({
+    payload: truthyvalue,
+    type: TOGGLE_CONFFIRMATION_CODE,
+});
+
+/** Triggers a function which changes a state variable
+ * this then indicates that the user has been authenticated
+ *
+ * @function
+ * @return {Object} The {@link actionTypes.AUTHENTICATE_USER_STATUS
+ *                                          AUTHENTICATE_USER_STATUS} action.
+ */
+const authenticateUserStatus = truthyvalue => ({
+    payload: truthyvalue,
+    type: AUTHENTICATE_USER_STATUS,
+});
+
+/** Triggers a function which changes a state variable
+ * this then indicates that the user has to confirm a code
+ * it triggers a saga which uses firebase to confirm
+ * if the code the user entered is what was sent to their phone
+ *
+ * @function
+ * @return {Object} The {@link actionTypes.CONFIRM_CODE CONFIRM_CODE} action.
+ */
+const confirmCode = code => ({
+    payload: code,
+    type: CONFIRM_CODE,
+});
+
+/** Triggers a function which checks the auth status of the user
+ * @function
+ * @return {Object} The {@link actionTypes.IS_USER_LOGGEDIN IS_USER_LOGGEDIN} action.
+ */
+const isUserAuthenticated = () => ({
+    type: IS_USER_LOGGEDIN,
+});
+
+// export the loginUser and authenticateUser as an object.
+export default {
+    authenticateUser,
+    authenticateUserStatus,
+    confirmCode,
+    confirmationCodeSection,
+    isUserAuthenticated,
+    loadingReducer,
+    loginUser,
+};
--- src/components/login/reducers.js
+++ src/components/login/reducers.js
@@ -1,13 +1,41 @@
-import { LOGIN_USER } from './actionTypes';
+// import { LOGIN_USER } from './actionTypes';
+import {
+    TOGGLE_LOADING_NIN,
+    TOGGLE_CONFFIRMATION_CODE,
+    AUTHENTICATE_USER_STATUS
+} from './actionTypes';
 
-const defaultPassword = '';
-const passwordReducer = (state = defaultPassword, action) => {
+// define the default state of the loader to be false
+
+const loading = false;
+
+// make the current state of loader to be false
+const loginLoandingReducer = (state = loading, action) => {
     switch (action.type) {
-    case LOGIN_USER:
+    case TOGGLE_LOADING_NIN:
         return action.payload;
     default:
         return state;
     }
 };
+
+const confirmReducer = (state = false, action) => {
+    switch (action.type) {
+    case TOGGLE_CONFFIRMATION_CODE:
+        return action.payload;
+    default:
+        return state;
+    }
+};
+
+const authenticateUserReducer = (state = false, action) => {
+    switch (action.type) {
+    case AUTHENTICATE_USER_STATUS:
+        return action.payload;
+    default:
+        return state;
+    }
+};
+
 // export this value
-export default passwordReducer;
+export default { authenticateUserReducer, confirmReducer, loginLoandingReducer };
