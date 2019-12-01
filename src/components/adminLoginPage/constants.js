f9e083a8e95e93ffc436270fe9eb479a818d79f7
Alexander <ashuaibu@getcarbon.co>
2019-12-01 16:30:18 +0100

    [add] auth

--- src/components/adminLoginPage/constants.js
+++ src/components/adminLoginPage/constants.js
@@ -1,3 +1,10 @@
 export const SIGNINHEADER = 'Admin Sign in';
 export const SIGNIN = 'LOG IN';
+export const WAIT_TIME = 3.0;
+export const ADMIN_ALREADY_LOGGED = 'You already logged in, please proceed to the dashboard';
+export const LOADING_MESSAGE = 'Authenticating Admin ....';
+export const SUCCESS_SIGNIN = 'Succesfully signed in';
+export const WHYWENEED = 'Sign in to your Admin Account';
+export const WHYNIN = `Proceed to sign into your admin account, 
+                        Rembember that your details are confidential and must not be shared!`;
 
