7b2197507870a658fa44b75be9b5d4efd3bb1429
Alexander <ashuaibu@getcarbon.co>
2019-12-01 15:41:55 +0100

    [add] auth

--- src/components/adminLoginPage/components/AdminLogin.js
+++ src/components/adminLoginPage/components/AdminLogin.js
@@ -1,7 +1,15 @@
 /* eslint-disable max-lines-per-function */
-import React, { useState } from 'react';
-import { Typography, Input, Button } from 'antd';
-import { SIGNINHEADER, SIGNIN } from '../constants';
+// import the modules from NIN
+import React, { useState, useEffect } from 'react';
+import {
+    Typography, Input, Button, Spin, Icon
+} from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+// import our modules
+import { SIGNINHEADER, SIGNIN, LOADING_MESSAGE } from '../constants';
+import actions from '../actions';
 
 import 'antd/dist/antd.css';
 
@@ -13,7 +21,14 @@ const { Password } = Input;
  * @return {jsx component} Login form
  */
 function AdminLogin() {
+    // get a state variables for out input
     const [state, setState] = useState({});
+    const dispatch = useDispatch();
+
+    // upon load of page check if the user is authenticated
+    useEffect(() => {
+        dispatch(actions.isAdminAuthenticated());
+    });
 
     /**
      * Handles input change
@@ -31,41 +46,64 @@ function AdminLogin() {
 
     const handleSubmit = event => {
         event.preventDefault();
+        // set the loader running
+        dispatch(actions.loadingAdmin(true));
+        // dispatch the login function
+        dispatch(actions.loginAdmin(state));
+        // clear all the fields
     };
 
+    // get the state curresponding to if the admin is loading
+    const adminLoading = useSelector(store => store.adminLoading);
+    // get all state to tell us if the user is authenticated
+    const adminAuthenticated = useSelector(store => store.adminAuthenticated);
+    // constant for customising the loader
+    const antIcon = <Icon type="loading" className="loader" spin />;
+
     return (
-        <form className="form" onSubmit={handleSubmit}>
-            <div className="form_subSection">
-                <Title className="form__heading" level={3}>{SIGNINHEADER}</Title>
-                {/* Email input field */}
-                <Input
-                    className="form__input"
-                    placeholder="Email"
-                    name="email"
-                    type="email"
-                    onChange={handleChange}
-                    required
-                />
-                {/* Password input field */}
-                <Password
-                    className="form__input"
-                    placeholder="Password"
-                    name="password"
-                    onChange={handleChange}
-                    required
-                />
-                <div className="-flex">
-                    {/* Submit button */}
-                    <Button
-                        className="form__submit"
-                        type="primary"
-                        htmlType="submit"
-                    >
-                        {SIGNIN}
-                    </Button>
-                </div>
-            </div>
-        </form>
+        <div>
+            {adminAuthenticated ? <Redirect to="/admindashboard" /> : <div />}
+            <form className="form" onSubmit={handleSubmit}>
+                <Spin
+                    size="large"
+                    indicator={antIcon}
+                    spinning={adminLoading}
+                    className="loader"
+                    tip={LOADING_MESSAGE}
+                >
+                    <div className="form_subSection">
+                        <Title className="form__heading" level={3}>{SIGNINHEADER}</Title>
+                        {/* Email input field */}
+                        <Input
+                            className="form__input"
+                            placeholder="Email"
+                            name="email"
+                            type="email"
+                            onChange={handleChange}
+                            required
+                        />
+                        {/* Password input field */}
+                        <Password
+                            className="form__input"
+                            placeholder="Password"
+                            name="password"
+                            onChange={handleChange}
+                            required
+                        />
+                        <div className="-flex">
+                            {/* Submit button */}
+                            <Button
+                                className="form__submit"
+                                type="primary"
+                                htmlType="submit"
+                            >
+                                {SIGNIN}
+                            </Button>
+                        </div>
+                    </div>
+                </Spin>
+            </form>
+        </div>
     );
 }
 
