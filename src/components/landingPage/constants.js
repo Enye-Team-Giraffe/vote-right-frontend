c67d0e32f1cb054cd3c5eee5c763798b9171e32b
Alexander <ashuaibu@getcarbon.co>
2019-12-08 02:54:34 +0100

    [add responsiveness and unity to layout]

--- src/components/landingPage/constants.js
+++ src/components/landingPage/constants.js
@@ -4,9 +4,43 @@ export const TWITTER_PAGE = 'www.twitter.com';
 export const YOUTUBE_PAGE = 'www.youtube.com';
 export const LOGO = 'voteRight';
 export const LOGINETXT = 'Login';
-export const FIRSTTITLE = 'Worldclass';
-export const SECONDTITLE = 'Blockchain Solution';
-export const HOMETEXT = `VoteRight aims to solve the challenge of voting in Nigeria.
-                        Ever gone to vote and almost gotten shot, mugged?
-Or maybe you spent hours on the Queue all to spend less than 10 minutes actually voting
-VoteRight to the rescue!!!`;
+
+const HOME = {
+    FIRSTTITLE: 'voteRight',
+    HOMETEXT: `VoteRight aims to solve the challenge of voting in Nigeria.
+    SECONDTITLE: 'What do we do?',
+    Ever gone to vote and almost gotten shot, mugged?
+    Or maybe you spent hours on the Queue 
+    all to spend less than 10 minutes actually voting
+    VoteRight to the rescue!!!`,
+    index: 0,
+};
+const ABOUT = {
+    FIRSTTITLE: 'ABOUT',
+    HOMETEXT: `VoteRight is a startup located in Nigeria
+    SECONDTITLE: 'Blockchain Solution',
+    which is located in Nigeria, At voteRIght 
+    we aim to solve the problems and insurgencies faced during the national election
+    using advanced technologies. `,
+    index: 1,
+};
+const PROBLEM = {
+    FIRSTTITLE: 'NATIONAL',
+    HOMETEXT: `Voting rates have reduced drastically by a whooping 15% rate in Nigeria,
+    SECONDTITLE: 'Voting Problem',
+    This is due to both the stress of commutation and lining up for hours
+    as well as health risks attached to voting in the country.
+    `,
+    index: 2,
+};
+const SOLUTION = {
+    FIRSTTITLE: 'Worldclass',
+    HOMETEXT: `A solution to this problem is to make voting mobile,
+    SECONDTITLE: 'Blockchain Solution',
+    This is made possible using several measures such as identity verification
+    as well as storing the data on the blockchain, where it is legally accessible
+    `,
+    index: 3,
+};
+
+export const MENUITEMS = [HOME, ABOUT, PROBLEM, SOLUTION];
