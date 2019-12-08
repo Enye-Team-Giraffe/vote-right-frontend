9d0b5b948053943c2c2487057ec874d82ed73b0c
Alexander <ashuaibu@getcarbon.co>
2019-12-08 03:09:09 +0100

    [bug]adjust homepage text options

--- src/components/landingPage/constants.js
+++ src/components/landingPage/constants.js
@@ -8,38 +8,38 @@ export const LOGINETXT = 'Login';
 const HOME = {
     FIRSTTITLE: 'voteRight',
     HOMETEXT: `VoteRight aims to solve the challenge of voting in Nigeria.
-    SECONDTITLE: 'What do we do?',
     Ever gone to vote and almost gotten shot, mugged?
     Or maybe you spent hours on the Queue 
     all to spend less than 10 minutes actually voting
     VoteRight to the rescue!!!`,
+    SECONDTITLE: 'What do we do?',
     index: 0,
 };
 const ABOUT = {
     FIRSTTITLE: 'ABOUT',
     HOMETEXT: `VoteRight is a startup located in Nigeria
-    SECONDTITLE: 'Blockchain Solution',
     which is located in Nigeria, At voteRIght 
     we aim to solve the problems and insurgencies faced during the national election
     using advanced technologies. `,
+    SECONDTITLE: 'Blockchain Solution',
     index: 1,
 };
 const PROBLEM = {
     FIRSTTITLE: 'NATIONAL',
     HOMETEXT: `Voting rates have reduced drastically by a whooping 15% rate in Nigeria,
-    SECONDTITLE: 'Voting Problem',
     This is due to both the stress of commutation and lining up for hours
     as well as health risks attached to voting in the country.
     `,
+    SECONDTITLE: 'Voting Problem',
     index: 2,
 };
 const SOLUTION = {
     FIRSTTITLE: 'Worldclass',
     HOMETEXT: `A solution to this problem is to make voting mobile,
-    SECONDTITLE: 'Blockchain Solution',
     This is made possible using several measures such as identity verification
     as well as storing the data on the blockchain, where it is legally accessible
     `,
+    SECONDTITLE: 'Blockchain Solution',
     index: 3,
 };
 
