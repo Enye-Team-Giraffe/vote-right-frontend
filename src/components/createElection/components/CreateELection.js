cfc680bbae24acd6c2dd22f18eec6b5d817260e5
usmanoa <usmanakorede@gmail.com>
2019-12-10 03:03:14 +0100

    Add input handlers

--- src/components/createElection/components/CreateELection.js
+++ src/components/createElection/components/CreateELection.js
@@ -3,36 +3,98 @@ import React, { useState } from 'react';
 import {
     Card, Input, DatePicker, Button
 } from 'antd';
-
 import './CreateElection.css';
 
+/**
+ * Form for creating election
+ *
+ * @component
+ * @return {component} - Component for creating election
+ */
 const CreateElection = () => {
+    const [name, updateName] = useState('');
+    const [description, updateDescription] = useState('');
+    const [startDate, updateStartDate] = useState('');
+    const [endDate, updateEndDate] = useState('');
 
-    const handleSubmit = (event) => {
+    /**
+     * Handles change in input text field
+     * @function
+     * @param {event} - Event on input field
+     */
+    const handleChangeText = ({ target }) => {
+        if (target.name === 'name') {
+            updateName(target.value);
+        } else {
+            updateDescription(target.value);
+        }
+    };
+
+    /**
+     * Handles change in start date datepicker
+     * @function
+     * @param {date} - selected date
+     */
+    const handleStartDate = date => {
+        const formattedDate = date.format('L');
+        updateStartDate(formattedDate);
+    };
+
+    /**
+     * Handles change end date datepicker
+     * @function
+     * @param {date} - selected date
+     */
+    const handleEndDate = date => {
+        const formattedDate = date.format('L');
+        updateEndDate(formattedDate);
+    };
+
+    /**
+     * Handles submission of form data
+     * @function
+     * @param {event} - event triggered by button click
+     */
+    const handleSubmit = event => {
         event.preventDefault();
-        
-    }
+        // eslint-disable-next-line no-unused-vars
+        const payload = {
+            description, endDate, name, startDate,
+        };
+    };
 
-    return(
+    return (
         <div className="createElection">
             <form className="createElectionForm" onSubmit={handleSubmit}>
                 <Card className="createElectionCard">
                     <h1 className="createElectionForm__heading">Create Election</h1>
                     <span>Name</span>
-                    <Input className="createElectionForm__input" />
+                    <Input
+                        className="createElectionForm__input"
+                        name="name"
+                        required
+                        onChange={handleChangeText}
+                    />
                     <span>Description</span>
-                    <Input.TextArea className="createElectionForm__textArea" />
+                    <Input.TextArea
+                        className="createElectionForm__textArea"
+                        name="description"
+                        required
+                        onChange={handleChangeText}
+                    />
                     <span className="-topMargin">Start date</span>
                     <DatePicker
                         className="createElectionForm__datePicker"
-                        showTime
-                        placeholder="Select time"
+                        placeholder="Select date"
+                        required
+                        onChange={handleStartDate}
                     />
                     <span>End date</span>
                     <DatePicker
                         className="createElectionForm__datePicker"
-                        showTime
-                        placeholder="Select time"
+                        placeholder="Select date"
+                        required
+                        onChange={handleEndDate}
                     />
                     <Button
                         className="createElectionForm__button"
@@ -43,7 +105,7 @@ const CreateElection = () => {
                 </Card>
             </form>
         </div>
-    )
+    );
 };
 
 export default CreateElection;
