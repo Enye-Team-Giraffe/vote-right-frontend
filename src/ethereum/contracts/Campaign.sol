1a9f0482651d3259b978e5322c2c7e0466a1c6a4
Alexander <ashuaibu@getcarbon.co>
2019-12-11 15:24:42 +0100

    [bug] reduce commit to 250

--- src/ethereum/contracts/Campaign.sol
+++ src/ethereum/contracts/Campaign.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.4.17;
 // define a new factory which would be responsibel for deploying a new instance
 // of an election
 contract ElectionFactory{
-    
     struct Summary{
         address location;//the address of this newly deployed election
         string name; //generated id for this candidate
@@ -39,29 +38,18 @@ contract ElectionFactory{
         return summaries.length;
     }
     
-    
 }
-
-
-
-
-
 // define a  function/contract which is reponsible for handling each election, this of course includes its candidates and results.
 contract Election {
-    
     ///////////////////////////////////////////////// initialise the variables and structs we are to use duting the election ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     // an election consists of two characteristics,
     // the candidates and the voters, 
-    // and we need to create a proper representation
-    
-    // this defines what properties a voter should look like.
     struct Voter {
         uint age;   // Age of the candidate
         string gender; // the gender of the voter
         string latlong;  // the latitude and longitude of the vote  lat_long
         string votedCandidate; // person delegated to
     }
-    
     // this defines what a candidate should  like
     struct Candidate{
         string id; //generated id for this candidate
@@ -73,30 +61,23 @@ contract Election {
         string education; //the highest certificate of said candidate
         int voteCount;
     }
-    
     // create a mapping to keep track of the candidate who have voted
     mapping(string=>bool) voted;
     mapping(string=>uint) indexes;
-    
     // create an array to keep track of all the candidates we have
     Candidate[] public candidates;
-    
     // create an array to keep track of all the people who have voted
     Voter[] public voters;
-    
     // create a variable which keeps track of the current state of the election
     // it could either by 1 for initialised ,2 for started or 3 for ended
     int public electionState;
-    
     // create a function to keep track of the manager of this election
     address public manager;
-    
     // create descriptive details about the election
     string  nameOfElection;
     string  description;
     uint  startdate;
     uint enddate;
-    
     // create a basic function that helps returns basic info about this election
     function aboutElection() public view returns (string,string,uint,uint){
         return (nameOfElection,description,startdate,enddate);
@@ -107,10 +88,7 @@ contract Election {
         require(msg.sender == manager);
         _;
     }
-    
-    // create a function to initialise the election
     // this serves as a constrictor for this contract, and assigns a manager to this contract
-    // also initialise the contract
     function Election(address creator,string electionName,string electionDescription,uint electionStartdate, uint electionEnddate) public{
         manager=creator;
         nameOfElection=electionName;
@@ -119,7 +97,6 @@ contract Election {
         enddate=electionEnddate;
         electionState=1;
     }
-    
     // first thing to do in an election is to create add to the array of candidates
     function addCandidate( string id, string name, uint age, string party,string quote, string pictureLink,string education ) public restricted{
         // make sure that the current state of the election is initialised
@@ -147,7 +124,6 @@ contract Election {
         electionState = 2;
     }
     
-           
     // after done adding candidates, next thing is to begin voting
     // everytime they vote add info about the voter
     function vote(string candidateId, uint age,string gender,string latlong,string phoneNumber) public {
@@ -188,11 +164,7 @@ contract Election {
     function concludeVoting() public{
         electionState = 3;
     }
-    
-    
-    
-    ////////////////////////////////////////////////////////////////// utility functions //////////////////////////////////////////////////////////////////////////
-    
+          
     // function to get the length of the candidates
     function getCandidatesLength() public view returns (uint){
         return candidates.length;
