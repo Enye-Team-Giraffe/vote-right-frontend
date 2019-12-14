cbe4f96e6f5e34129b03afcc204992e1f56137ee
Alexander <ashuaibu@getcarbon.co>
2019-12-14 18:42:10 +0100

    [bug] make changes to 250

--- src/components/viewStats/actions.js
+++ src/components/viewStats/actions.js
@@ -2,22 +2,18 @@ import {
     LOAD_VOTERS, PUSH_VOTERS,
     LOADING_VOTERS
 } from './actionTypes';
-
 const loadVoters = electionId => ({
     payload: electionId,
     type: LOAD_VOTERS,
 });
-
 const pushVoters = votersArray => ({
     payload: votersArray,
     type: PUSH_VOTERS,
 });
-
 const loadingVoters = truthyValue => ({
     payload: truthyValue,
     type: LOADING_VOTERS,
 });
-
 export default {
     loadVoters,
     loadingVoters,
