09731d90ca0e84e9ff24a99cab6d194be09a9be4
Alexander <ashuaibu@getcarbon.co>
2019-12-14 20:50:57 +0100

    [add] comments to actions

--- src/components/viewStats/actionTypes.js
+++ src/components/viewStats/actionTypes.js
@@ -1,7 +1,42 @@
+/**
+ * Fired by the {@link actions.loadVoters loginUser}
+ * action creator.
+ *
+ * @type {String}
+ */
 export const LOAD_VOTERS = 'LOAD_VOTERS';
+/**
+ * Fired by the {@link actions.loadingVoters loginUser}
+ * action creator.
+ *
+ * @type {String}
+ */
 export const LOADING_VOTERS = 'LOADING_VOTERS';
+/**
+ * Fired by the {@link actions.pushVoters loginUser}
+ * action creator.
+ *
+ * @type {String}
+ */
 export const PUSH_VOTERS = 'PUSH_VOTERS';
-
+/**
+ * Fired by the {@link actions.loadCandidates loginUser}
+ * action creator.
+ *
+ * @type {String}
+ */
 export const LOAD_CANDIDATES = 'LOAD_CANDIDATES';
+/**
+ * Fired by the {@link actions.loadingCandidates loginUser}
+ * action creator.
+ *
+ * @type {String}
+ */
 export const LOADING_CANDIDATES = 'LOADING_CANDIDATES';
+/**
+ * Fired by the {@link actions.pushCandidates loginUser}
+ * action creator.
+ *
+ * @type {String}
+ */
 export const PUSH_CANDIDATES = 'PUSH_CANDIDATES';
