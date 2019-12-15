21fe07c6582cc8b06aa58443575f05919b26b9e0
usmanoa <usmanakorede@gmail.com>
2019-12-15 15:40:07 +0100

    Add validation checks for image

--- src/components/addCandidate/components/AddCandidate.js
+++ src/components/addCandidate/components/AddCandidate.js
@@ -1,7 +1,11 @@
 /* eslint-disable max-lines-per-function */
 import React, { useState, useEffect } from 'react';
 import {
+<<<<<<< HEAD
     Card, Input, DatePicker, Button, Upload, Icon, Spin
+=======
+    Card, Input, DatePicker, Button, Upload, Icon, message
+>>>>>>> Add validation checks for image
 } from 'antd';
 import './AddCandidate.css';
 
@@ -28,15 +32,62 @@ const AddCandidate = ({ match }) => {
     const [imageUrl, updateImageUrl] = useState(null);
     const dispatch = useDispatch();
 
+
+    /**
+     * Get 64Base url for image
+     * @function
+     * @param {file} - file to be read
+     * @param {function} - callback to set imageUrl
+     * @return {boolean}
+     */
+    const getBase64 = (file, setUrl) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => setUrl(reader.result);
+    };
+/**
+     * Checkes whether image is valid for uploading with warning
+     * @function
+     * @param {file} - file to be uploaded
+     * @return {boolean}
+     */
+    const isValidImage = file => {
+        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/jpg'
+            || file.type === 'image/png';
+        if (!isJpgOrPng) {
+            message.error('You can only upload JPG/PNG file!');
+        }
+        const fileSize = file.size / 1024 / 1024 < 2;
+        if (!fileSize) {
+            message.error('Image must not be larger than 2MB!');
+        }
+        return isJpgOrPng && fileSize;
+    };
+
+    /**
+     * Checkes whether image is valid for uploading without
+     * displaying warning
+     * @function
+     * @param {file} - file to be uploaded
+     * @return {boolean}
+     */
+    const isValidImageWithoutWarning = file => {
+        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/jpg'
+            || file.type === 'image/png';
+        const fileSize = file.size / 1024 / 1024 < 2;
+        return isJpgOrPng && fileSize;
+    };
+
     /**
      * Handles change in image
      * @function
      * @param {event} - Event on upload field
      */
     const handleChangeImage = info => {
-        const reader = new FileReader();
-        reader.readAsDataURL(info.file.originFileObj);
-        reader.onload = () => updateImageUrl(reader.result);
+        if (!isValidImageWithoutWarning(info.file)) {
+            return;
+        }
+        getBase64(info.file.originFileObj, updateImageUrl);
     };
 
     /**
@@ -102,6 +153,7 @@ const AddCandidate = ({ match }) => {
     return (
         <div className="addCandidate">
             <form className="addCandidateForm" onSubmit={handleSubmit}>
+<<<<<<< HEAD
                 <Spin
                     size="large"
                     indicator={antIcon}
@@ -193,6 +245,94 @@ const AddCandidate = ({ match }) => {
                         </Button>
                     </Card>
                 </Spin>
+=======
+
+                <Card className="addCandidateCard">
+                    <div className="addCandidateForm__heading">
+                        <h1 className="addCandidateForm__heading_header">{ADD_CANDIDATE}</h1>
+                        <p className="addCandidateForm__heading_text">
+                            {HEADING}
+                        </p>
+                    </div>
+                    <div className="addCandidateForm__image">
+                        <Card className="-card">
+                            <Upload
+                                name="avatar"
+                                className="avatar-uploader"
+                                showUploadList={false}
+                                beforeUpload={isValidImage}
+                                onChange={handleChangeImage}
+                            >
+                                {
+                                    imageUrl
+                                        ? <img src={imageUrl} alt="avatar" />
+                                        : (
+                                            <Button>
+                                                <Icon type="upload" />
+                                                <span className="ant-upload-text">
+                                                    {UPLOAD_IMAGE}
+                                                </span>
+                                            </Button>
+                                        )
+                                }
+                            </Upload>
+                        </Card>
+                    </div>
+                    <div className="addCandidateForm__group">
+                        <span>{NAME}</span>
+                        <Input
+                            className="addCandidateForm__input --input-element"
+                            name="name"
+                            required
+                            onChange={handleChangeText}
+                        />
+                    </div>
+                    <div className="addCandidateForm__group">
+                        <span className="-topMargin">{DATE_OF_BIRTH}</span>
+                        <DatePicker
+                            className="addCandidateForm__datePicker --input-element"
+                            placeholder="Select date"
+                            required
+                            onChange={handleDateOfBirth}
+                        />
+                    </div>
+                    <div className="addCandidateForm__group">
+                        <span>{EDUCATION}</span>
+                        <Input
+                            className="addCandidateForm__input --input-element"
+                            name="education"
+                            required
+                            onChange={handleChangeText}
+                        />
+                    </div>
+                    <div className="addCandidateForm__group">
+                        <span>{PARTY}</span>
+                        <Input
+                            className="addCandidateForm__input --input-element"
+                            name="party"
+                            required
+                            onChange={handleChangeText}
+                        />
+                    </div>
+                    <div className="addCandidateForm__group">
+                        <span>{QUOTE}</span>
+                        <Input.TextArea
+                            rows={ROW_HEIGHT}
+                            className="addCandidateForm__textArea --input-element"
+                            name="quote"
+                            required
+                            onChange={handleChangeText}
+                        />
+                    </div>
+                    <Button
+                        className="addCandidateForm__button"
+                        htmlType="submit"
+                    >
+                        {BUTTON_TEXT}
+                    </Button>
+                </Card>
+
+>>>>>>> Add validation checks for image
             </form>
         </div>
     );
