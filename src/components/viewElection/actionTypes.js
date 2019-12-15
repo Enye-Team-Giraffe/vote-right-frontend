3f6c31ee14c813e0d2d495277a916972104cd6f2
Alexander <ashuaibu@getcarbon.co>
2019-12-15 11:02:45 +0100

    [add]comments to sagas

--- src/components/viewElection/actionTypes.js
+++ src/components/viewElection/actionTypes.js
@@ -1,3 +1,18 @@
+/**
+ * Fired by the {@link actions.loadElections loginUser}
+ * action creator.
+ * @type {String}
+ */
 export const LOAD_ELECTIONS = 'LOAD_ELECTIONS';
+/**
+ * Fired by the {@link actions.pushElections loginUser}
+ * action creator.
+ * @type {String}
+ */
 export const PUSH_ELECTIONS = 'PUSH_ELECTIONS';
+/**
+ * Fired by the {@link actions.loadingElections loginUser}
+ * action creator.
+ * @type {String}
+ */
 export const LOADING_ELECTION = 'LOADING_ELECTION';
--- src/components/viewElection/actions.js
+++ src/components/viewElection/actions.js
@@ -3,15 +3,28 @@ import {
     LOADING_ELECTION
 } from './actionTypes';
 
+/**
+ * Triggers request to spin indication that it is currently performing an async
+ * @function
+ * @return {Object} The {@link actionTypes.LOAD_ELECTIONS LOAD_ELECTIONS} action.
+ */
 const loadElections = () => ({
     type: LOAD_ELECTIONS,
 });
-
+/**
+ * Triggers request to spin indication that it is currently performing an async
+ * @function
+ * @return {Object} The {@link actionTypes.PUSH_ELECTIONS PUSH_ELECTIONS} action.
+ */
 const pushElections = electionsArray => ({
     payload: electionsArray,
     type: PUSH_ELECTIONS,
 });
-
+/**
+ * Triggers request to spin indication that it is currently performing an async
+ * @function
+ * @return {Object} The {@link actionTypes.LOADING_ELECTION LOADING_ELECTION} action.
+ */
 const loadingElections = truthyValue => ({
     payload: truthyValue,
     type: LOADING_ELECTION,
