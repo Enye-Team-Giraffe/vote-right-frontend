a18dd2cb9c02fc4b5ac4e2c57f2cc52e625157bf
Alexander <ashuaibu@getcarbon.co>
2020-01-22 17:16:01 +0100

    [bug] remove credentials from git

--- src/components/configuredFirebase/config.js
+++ src/components/configuredFirebase/config.js
@@ -3,11 +3,14 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    measurementId: "G-1X6BMC89T8",
-    // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 };
+
+if (firebaseConfig['measurementId'] === undefined){
+    firebaseConfig['measurementId']="1234"
+}
 
 export default firebaseConfig;
