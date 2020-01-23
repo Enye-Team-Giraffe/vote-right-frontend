7fe8a916a098f74db631ba0f8781c120f091af18
Alexander <ashuaibu@getcarbon.co>
2020-01-23 16:17:19 +0100

    [add]particles to homepage

--- src/components/viewElection/components/ViewElection.js
+++ src/components/viewElection/components/ViewElection.js
@@ -9,13 +9,16 @@ import {
 import { NavLink } from 'react-router-dom';
 import {
     LOADING_MESSAGE, NO_RUNNING_ELECTION,
-    ADMIN_VIEW_ONGOING_ELECTIONS
+    ADMIN_VIEW_ONGOING_ELECTIONS, PARTICLE_STYLE
 } from '../constants';
 import { analytics } from '../../configuredFirebase';
 import actions from '../actions';
 import Particles from 'react-particles-js';
+import { particleParameters } from '../constants';
+
 
 const { Meta } = Card;
+
 
 //  a function to display the title of the card
 const CardTitle = ({ title }) => (
@@ -124,7 +127,10 @@ export default function ViewElection() {
     const toDateString = tstamp => new Date(Number(tstamp) * 1000).toDateString().slice(0, 15);
     return (
         <div className="viewElectionLayout">
-            <Particles/>
+            <Particles
+                params={particleParameters}
+                style={PARTICLE_STYLE}
+            />
             <Spin
                 size="large"
                 indicator={antIcon}
@@ -210,54 +216,3 @@ CardFooter.propTypes = {
     endDate: PropTypes.string.isRequired,
 };
 
-
-import React from 'react';
-import Particles from 'react-particles-js';
-import './style.css';
-import { particleParameters } from './constants';
-
-// style for the item which we put over the animation
-const overlay = {
-    alignItems: 'center',
-    color: 'white',
-    display: 'flex',
-    fontSize: '20px',
-    height: '100%',
-    justifyContent: 'center',
-};
-
-// style for the particle which we want to display
-const particleStyle = {
-    height: '100%',
-    left: 0,
-    paddingBottom: '3%',
-    paddingTop: '3%',
-    position: 'absolute',
-    top: 0,
-    width: '100%',
-};
-
-/**
- * Default layout for pages
- *
- * @function
- * @param {func} ComponentOne - one of two component to display
- * @param {func} ComponentTwo - one of two component to display
- * @return {Component} the jsx component for default page layout
- */
-const withLayout = (ComponentOne = 'div', ComponentTwo = 'div') => () => (
-    <div className="layout">
-        <div className="whiteSection">
-            <ComponentOne />
-        </div>
-        <div className="blueSection -blue">
-            <ComponentTwo style={overlay} />
-            <Particles
-                params={particleParameters}
-                style={particleStyle}
-            />
-        </div>
-    </div>
-);
-
-export default withLayout;
--- src/components/viewElection/constants.js
+++ src/components/viewElection/constants.js
@@ -4,3 +4,61 @@ export const LOADING_MESSAGE = 'Loading Elections from the blockchain...';
 export const META_ITEM = [{}];
 export const NO_RUNNING_ELECTION = 'No Running election';
 export const ADMIN_VIEW_ONGOING_ELECTIONS = 'ADMIN_VIEW_ONGOING_ELECTIONS';
+export const PARTICLE_STYLE = {
+    height: '100%',
+    left: 0,
+    paddingTop: '12%',
+    position: 'absolute',
+    display:"flex",
+    justifyContent:"center",
+    top: 0,
+};
+export const particleParameters = {
+    particles: {
+        color: {
+            value: ['#05052d'],
+        },
+        line_linked: {
+            color: '#0D3578',
+            distance: 200,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+        },
+        number: {
+            density: {
+                enable: true,
+                value_area: 700,
+            },
+            value: 13,
+        },
+        opacity: {
+            anim: {
+                enable: false,
+                opacity_min: 0.1,
+                speed: 1,
+                sync: false,
+            },
+            random: true,
+            value: 0.7,
+        },
+        shape: {
+            polygon: {
+                nb_sides: 2,
+            },
+            stroke: {
+                color: '#145ea8',
+                width: 1,
+            },
+            type: 'circle',
+        },
+        move: {
+            enable: false,
+        },
+        size: {
+            random: true,
+            value: 10,
+        },
+    },
+    retina_detect: true,
+};
