fea8ae9cf1687b4e9c8c1522fef88214f772a40f
Alexander <ashuaibu@getcarbon.co>
2020-02-01 15:42:52 +0100

    [bug] lint test

--- src/web3/configuredWeb3.js
+++ src/web3/configuredWeb3.js
@@ -2,8 +2,8 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 
 const provider = new HDWalletProvider(
-    process.env.REACT_APP_ETHEREUM_MNEMONIC,
-    process.env.REACT_APP_ETHEREUM_INFURA_KEY
+    'zero apart reform rack west fire ramp twist popular repair envelope short',
+    'https://rinkeby.infura.io/v3/727e5c1247204e3190766708acfebb4d'
 );
 
 const web3 = new Web3(provider);
